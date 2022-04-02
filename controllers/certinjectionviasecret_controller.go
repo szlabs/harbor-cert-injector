74a72ce6e0a17e21384c45e164f29da6fe9d97c5
Steven Zou <szou@vmware.com>
2022-04-02 19:24:07 +0800

    fix(rbac): fix secrets rbac settings

--- controllers/certinjectionviasecret_controller.go
+++ controllers/certinjectionviasecret_controller.go
@@ -42,7 +42,9 @@ type CertInjectionViaSecretReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=secrets/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups="",resources=secrets/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
